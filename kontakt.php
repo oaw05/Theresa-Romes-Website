ee898dd41c07dbc827085092e0be9cf6d81e6731
oaw05 <th_andi@gmx.net>
2016-06-16 21:28:09 +0200

    status quo
    This is what I've done so far.

---
+++ kontakt.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+	<head lang="de">
+		<title>Home - Theresa Maria Romes</title>
+		<meta charset="utf-8">
+		
+		<!-- CSS einbinden -->
+		<link href="style.css" rel="stylesheet" type="text/css">
+		
+		<!-- Google Fonts einbinden -->
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic|Istok+Web:400,400italic,700' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Lobster+Two:400,700|Satisfy|Playball|Great+Vibes|Parisienne' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div class="container">
+			<aside>
+			
+				<!-- Navigation & Bild -->
+				<div class="nav_bild_container">	
+					<img src="img/Kontakt.png" alt="Kontakt" class="nav_bild">	
+				</div>
+				
+				<nav class="navigation vertical">
+					<ul>
+						<li>
+							<a href="home.php" title="Home">Home</a>
+						</li>
+						<li>
+							<a href="vita.php" title="Vita">Vita</a>
+						</li>
+						<li>
+							<a href="kalender.php" title="Kalender">Kalender</a>
+						</li>
+						<li>
+							<a href="repertoire.php" title="Repertoire">Repertoire</a>
+						</li>
+						<li>
+							<a href="media.php" title="Media">Media</a>
+						</li>
+						<li>
+							<a href="presse.php" title="Presse">Presse</a>
+						</li>
+						<li>
+							<a href="kontakt.php" title="Kontakt" class="active">Kontakt</a>
+						</li>
+					</ul>
+				</nav>
+				<!-- Textur unten -->
+				<div class="background">
+					<img src="img/Background3.png" alt="textur_unten" class="textur_background">
+				</div>
+			</aside>
+			
+			<div class="right_column">
+				<!-- LOGO --> 
+				<div>
+					<a href="home.php" class="header_link">
+						<div class="header">
+							<h1>Theresa Maria Romes</h1>
+							<h2>Sopran</h2>
+						</div>
+					</a>
+				</div>
+
+				<!-- INHALT -->
+				<div class="inhalt">
+					<div class="impressum_rechts">
+						<h3>Impressum</h3>
+						<p><b>Inhaltlich Verantwortlicher:</b><br>
+						Theresa Romes (Anschrift: siehe Kontakt)</p>
+						<p><b>Gestaltung:</b><br>
+						Andrea Reidel & Theresa Romes</p>
+						<p><b>Umsetzung:</b><br>
+						Andrea Reidel</p>
+						<p><b>Fotograf:</b><br>
+						Daniel M.G. Weiß</p>
+						<p><b>Haftungshinweis:</b><br>
+						Trotz sorgfältiger inhaltlicher Kontrolle 
+						übernehme ich keine Haftung für die Inhalte
+						externer Links. Für den Inhalt der verlinkten
+						Seiten sind ausschließlich deren Betreiber
+						verantwortlich.</p>
+					</div>
+					<div class="impressum_links">
+						<h3>Kontakt</h3>
+						<p>Theresa Romes<br>
+						Leonhardstraße 11<br>
+						89567 Sontheim<br><br>
+						theresa.romes@freenet.de</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
